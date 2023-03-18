53420d88bdae045409274e6eb4eade305a68b175
saulmamani <luas0_1@yahoo.es>
2023-03-18 07:39:57 -0400

    Clase Auxiliar agregada

---
+++ ClasesHerencia/Auxiliar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesHerencia
+{
+    internal class Auxiliar : Usuario
+    {
+        /// <summary>
+        /// Sueldo del docente
+        /// </summary>
+        public decimal Sueldo { get; set; }
+        public int horas { get; set; }
+
+        public override string ToString()
+        {
+            return $"Cuenta: {Cuenta} | Pass: {Password} | Nombre: {Nombre} | Sueldo: {Sueldo}";
+        }
+    }
+}
